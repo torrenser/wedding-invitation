ded0a3e2f66c458b7153789574c5f7898c53c857
torrenser <torrenser@naver.com>
2026-06-28 08:49:44 +0900

    신랑 신부 계좌번호 수정

--- src/const.ts
+++ src/const.ts
@@ -40,19 +40,19 @@ export const BRIDE_INFO = [
     relation: "신부",
     name: BRIDE_FULLNAME,
     phone: "010-0000-0000",
-    account: "우리은행 0000000000000",
+    account: "지역농축협 356049-42-84643",
   },
   {
     relation: "신부 아버지",
     name: BRIDE_FATHER,
     phone: "010-0000-0000",
-    account: "하나은행 00000000000",
+    account: "국민은행 022-21-0621-593",
   },
   {
     relation: "신부 어머니",
     name: BRIDE_MOTHER,
     phone: "010-0000-0000",
-    account: "하나은행 00000000000000",
+    account: "하나은행 168-890402-48407",
   },
 ]
 
@@ -66,19 +66,19 @@ export const GROOM_INFO = [
     relation: "신랑",
     name: GROOM_FULLNAME,
     phone: "010-0000-0000",
-    account: "케이뱅크 00000000000000",
+    account: "케이뱅크 100-102-700086",
   },
   {
     relation: "신랑 아버지",
     name: GROOM_FATHER,
     phone: "010-0000-0000",
-    account: "신한은행 000000000000",
+    account: "지역농협 173555-56-142901",
   },
   {
     relation: "신랑 어머니",
     name: GROOM_MOTHER,
     phone: "010-0000-0000",
-    account: "국민은행 000000000000",
+    account: "토스뱅크 1000-3518-6592",
   },
 ]
 
