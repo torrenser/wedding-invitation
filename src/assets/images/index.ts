df71b64a3504a2042695b55ed996f2ede1a81b44
torrenser <torrenser@naver.com>
2026-06-27 18:42:32 +0900

    실제 웨딩 이미지로 변경

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,16 +1,24 @@
 import coverImage from "./cover.png"
-import image1 from "./image1.png"
-import image2 from "./image2.png"
-import image3 from "./image3.png"
-import image4 from "./image4.png"
-import image5 from "./image5.png"
-import image6 from "./image6.png"
-import image7 from "./image7.png"
-import image8 from "./image8.png"
-import image9 from "./image9.png"
-import image10 from "./image10.png"
-import image11 from "./image11.png"
-import image12 from "./image12.png"
+import image1 from "./image1.jpg"
+import image2 from "./image2.jpg"
+import image3 from "./image3.jpg"
+import image4 from "./image4.jpg"
+import image5 from "./image5.jpg"
+import image6 from "./image6.jpg"
+import image7 from "./image7.jpg"
+import image8 from "./image8.jpg"
+import image9 from "./image9.jpg"
+import image10 from "./image10.jpg"
+import image11 from "./image11.jpg"
+import image12 from "./image12.jpg"
+import image13 from "./image13.jpg"
+import image14 from "./image14.jpg"
+import image15 from "./image15.jpg"
+import image16 from "./image16.jpg"
+import image17 from "./image17.jpg"
+import image18 from "./image18.jpg"
+import image19 from "./image19.jpg"
+import image20 from "./image20.jpg"
 
 export const COVER_IMAGE = coverImage
 
@@ -27,4 +35,12 @@ export const GALLERY_IMAGES = [
   image10,
   image11,
   image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17,
+  image18,
+  image19,
+  image20
 ]
